d25aaa68140b9b956dbf77c823d1931e51f3c856
muchmallow <31728584+muchmallow@users.noreply.github.com>
2019-11-02 17:26:49 +0200

    tags

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -26,13 +26,19 @@ const drawerWidth = 315;
 
 const useStyles = makeStyles(theme => ({
     root: {
+        width: '100%'
+    },
+    wrapper: {
+        maxWidth: '1440px',
         flexGrow: 1,
-        display: 'flex'
+        display: 'flex',
+        margin: '0 auto',
+        position: 'relative'
     },
     appBar: {
-        backgroundColor: "#3a8378",
-        height: "51px",
-        justifyContent: "center",
+        backgroundColor: '#3a8378',
+        height: '51px',
+        justifyContent: 'center',
         transition: theme.transitions.create(['margin', 'width'], {
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.leavingScreen,
@@ -101,7 +107,9 @@ const useStyles = makeStyles(theme => ({
         flexShrink: 0,
     },
     drawerPaper: {
+        position: "relative",
         width: drawerWidth,
+        border: "none"
     },
     drawerHeader: {
         display: 'flex',
@@ -158,122 +166,106 @@ export default function Menu() {
 
     return (
         <div className={classes.root}>
-            <CssBaseline/>
-            <AppBar
-                position="fixed"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}
-            >
-                <Toolbar>
-                    {open ? <IconButton onClick={handleDrawerClose}
-                                        color="inherit"
-                                        edge="start"
-                                        className={classes.menuButton}>
-                            {theme.direction === 'ltr' && <ChevronLeftIcon/>}
-                        </IconButton>
-                        : <IconButton
-                            color="inherit"
-                            aria-label="open drawer"
-                            onClick={handleDrawerOpen}
-                            edge="start"
-                            className={classes.menuButton}
-                        >
-                            <MenuIcon/>
-                        </IconButton>}
-                    <Typography className={classes.title} variant="h6" noWrap>
-                        Logo
-                    </Typography>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon/>
+            <div className={classes.wrapper}>
+                <CssBaseline/>
+                <AppBar
+                    position="absolute"
+                    className={clsx(classes.appBar, {
+                        [classes.appBarShift]: open,
+                    })}
+                >
+                    <Toolbar>
+                        {open ? <IconButton onClick={handleDrawerClose}
+                                            color="inherit"
+                                            edge="start"
+                                            className={classes.menuButton}>
+                                {theme.direction === 'ltr' && <ChevronLeftIcon/>}
+                            </IconButton>
+                            : <IconButton color="inherit"
+                                          aria-label="open drawer"
+                                          onClick={handleDrawerOpen}
+                                          edge="start"
+                                          className={classes.menuButton}>
+                                <MenuIcon/>
+                            </IconButton>}
+                        <Typography className={classes.title} variant="h6" noWrap>
+                            Logo
+                        </Typography>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon/>
+                            </div>
+                            <InputBase
+                                placeholder="Search…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                                inputProps={{'aria-label': 'search'}}
+                            />
                         </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{'aria-label': 'search'}}
-                        />
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    className={classes.drawer}
+                    variant="persistent"
+                    anchor="left"
+                    open={open}
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                >
+                    <div className={classes.drawerHeader}>
+                        {/*<IconButton onClick={handleDrawerClose}>*/}
+                        {/*    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}*/}
+                        {/*</IconButton>*/}
                     </div>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                className={classes.drawer}
-                variant="persistent"
-                anchor="left"
-                open={open}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-            >
-                <div className={classes.drawerHeader}>
-                    {/*<IconButton onClick={handleDrawerClose}>*/}
-                    {/*    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}*/}
-                    {/*</IconButton>*/}
-                </div>
-                <div className={classes.filters}>
-                    <Typography variant={'body2'}>Filters</Typography>
-                </div>
-                <Filters id='typePlaceholder' name='Type' placeholder='All Types'/>
-                <Filters id='usagePlaceholder' name='Usage' placeholder='All Usages'/>
-                <Filters id='agePlaceholder' name='Age' placeholder='All Ages'/>
-                <div className={classes.filters}>
-                    <Typography variant={'body2'}>Tags</Typography>
-                </div>
-                <Tags/>
-                <div className={classes.filters}>
-                    <Typography variant={'body2'}>Folders</Typography>
-                </div>
-                <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
-                            <ListItemText primary={text}/>
-                        </ListItem>
-                    ))}
-                </List>
-                <Divider/>
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
-                            <ListItemText primary={text}/>
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer>
-            <main
-                className={clsx(classes.content, {
-                    [classes.contentShift]: open,
-                })}
-            >
-                <div className={classes.mainHeader}/>
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-                </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-                </Typography>
-            </main>
+                    <div className={classes.filters}>
+                        <Typography variant={'body2'}>Filters</Typography>
+                    </div>
+                    <Filters id='typePlaceholder' name='Type' placeholder='All Types'/>
+                    <Filters id='usagePlaceholder' name='Usage' placeholder='All Usages'/>
+                    <Filters id='agePlaceholder' name='Age' placeholder='All Ages'/>
+                    <div className={classes.filters}>
+                        <Typography variant={'body2'}>Tags</Typography>
+                    </div>
+                    <Tags/>
+                    <div className={classes.filters}>
+                        <Typography variant={'body2'}>Folders</Typography>
+                    </div>
+                    <Divider/>
+                </Drawer>
+                <main
+                    className={clsx(classes.content, {
+                        [classes.contentShift]: open,
+                    })}
+                >
+                    <div className={classes.mainHeader}/>
+                    <Typography paragraph>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                        facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                        gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                        donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                        adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                        Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                        imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                        arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                        donec massa sapien faucibus et molestie ac.
+                    </Typography>
+                    <Typography paragraph>
+                        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+                        facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                        tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+                        consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+                        vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+                        hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+                        tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+                        nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+                        accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+                    </Typography>
+                </main>
+            </div>
         </div>
     );
 }
--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,72 +1,102 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Chip from '@material-ui/core/Chip';
 import Input from '@material-ui/core/Input';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import clsx from 'clsx';
-import IconButton from '@material-ui/core/IconButton';
-import FilledInput from '@material-ui/core/FilledInput';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
 
 const useStyles = makeStyles(theme => ({
     root: {
-        display: 'flex',
-        justifyContent: 'center',
-        flexWrap: 'wrap',
-        '& > *': {
-            margin: theme.spacing(0.5),
-        },
+        padding: "0px 11px 23px 16px"
+        // '& > *': {
+        //     margin: theme.spacing(0.5),
+        // },
     },
-    chip: {
-        margin: theme.spacing(0.5),
+    inputWrapper: {
+        display: "block",
+        width: "100%"
     },
     input: {
-        margin: theme.spacing(1),
+        width: '100%',
+        margin: '14px 2px 18px 0'
+    },
+    inputIcon: {
+        color: '#757575',
+        "&:hover": {
+            color: '#3a8378'
+        }
+    },
+    tagsWrapper: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        margin: 0,
+        padding: 0
+    },
+    chip: {
+        margin: '2px 2px 2px 2px',
+        transition: '0.3s',
+        "&:hover": {
+            border: "1px solid #3a8378"
+        }
     },
 }));
 
 const Tags = () => {
     const classes = useStyles();
-    const [chipData, setChipData] = React.useState([
-        {key: 0, label: 'Architecture'},
-        {key: 1, label: 'Plants'},
-        {key: 2, label: 'Animals'},
-        {key: 3, label: 'Movies'},
-        {key: 4, label: 'Top music drops'},
+    const [inputValue, setInputValue] = useState('');
+    const [tagList, setTagList] = useState([
+        'Architecture',
+        'Plants',
+        'Animals',
+        'Movies',
+        'Top music drops',
     ]);
 
-    const handleDelete = (chipToDelete) => () => {
-        setChipData((chips) =>
-            chips.filter(chip => chip.key !== chipToDelete.key));
+    const handleInput = (e) => {
+        console.log(e.target.value);
+        setInputValue(e.target.value.trim());
     };
 
-    const tags = chipData.map(data => <Chip key={data.key}
-                                            label={data.label}
-                                            onDelete={handleDelete(data)}
+    const addNewTag = () => {
+        if (!tagList.includes(inputValue)) {
+            setTagList([inputValue, ...tagList]);
+            setInputValue('');
+        }
+    };
+
+    const handleDelete = (tagToDelete) => () => {
+        setTagList((tagList) => tagList.filter(tag => tag !== tagToDelete));
+    };
+
+    const tags = tagList.map(tag => <Chip key={tag}
+                                            label={tag}
+                                            onDelete={handleDelete(tag)}
                                             className={classes.chip}
                                             variant="outlined"/>);
 
     return (
-        <>
-            <div className={classes.root}>
+        <div className={classes.root}>
+            <FormControl className={classes.inputWrapper}>
                 <Input id='addTag'
-                       placeholder="Placeholder"
+                       placeholder="Please enter a tag"
                        className={classes.input}
-                       inputProps={{
-                           'aria-label': 'description',
-                       }}
+                       onChange={handleInput}
+                       value={inputValue}
+                       endAdornment={
+                           <InputAdornment position='end'>
+                               <IconButton onClick={addNewTag}>
+                                   <AddCircleOutlineIcon className={classes.inputIcon}/>
+                               </IconButton>
+                           </InputAdornment>
+                       }
                 />
-                <AddCircleOutlineIcon/>
+            </FormControl>
+            <div className={classes.tagsWrapper}>
+                {tags}
             </div>
-            {tags}
-        </>
+        </div>
     );
 };
 
